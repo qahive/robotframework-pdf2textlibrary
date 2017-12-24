a1e6649730b209fda9699948730f2d02d0b9834d
penploy <penploy.s@gmail.com>
2017-12-24 16:50:24 +0700

    Update library

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from os.path import join, dirname
+
+execfile(join(dirname(__file__), 'src', 'Pdf2TextLibrary', 'version.py'))
+
+from distutils.core import setup
+
+CLASSIFIERS = """
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+long_description=open(join(dirname(__file__), 'README.md',)).read()
+
+setup(
+  name             = 'robotframework-pdf2textlibrary',
+  version          = VERSION,
+  description      = 'Robot Framework PDF Inspect Library',
+  long_description = long_description,
+  author           = 'bloopark systems GmbH & Co. KG',
+  author_email     = 'info@bloopark.com',
+  url              = 'https://github.com/qahive/robotframework-pdf2textlibrary',
+  license          = 'Apache License 2.0',
+  keywords         = 'robotframework read pdf reports',
+  platforms        = 'any',
+  zip_safe         = False,
+  classifiers      = CLASSIFIERS.splitlines(),
+  package_dir      = {'' : 'src'},
+  install_requires = ['robotframework', 'pdfminer'],
+  packages         = ['Pdf2TextLibrary'],
+)
---
+++ src/Pdf2TextLibrary/__init__.py
@@ -0,0 +1,50 @@
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from cStringIO import StringIO
+
+class Pdf2TextLibrary(object):
+    ROBOT_LIBRARY_SCOPE = 'Global'
+
+    def __init__(self):
+        print 'pdt to text library'
+
+    def convert_pdf_to_txt(self,path):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = file(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos=set()
+        for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
+            interpreter.process_page(page)
+        fp.close()
+        device.close()
+        str = retstr.getvalue()
+        retstr.close()
+        return str
+
+    def count_pdf_pages(self,path):
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        fp = file(path, 'rb')
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        password = ""
+        maxpages = 0
+        caching = True
+        pagenos=set()
+        pagenumber=0
+        for page in enumerate(PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True)):
+            pagenumber=pagenumber+1
+        fp.close()
+        device.close()
+        return pagenumber
